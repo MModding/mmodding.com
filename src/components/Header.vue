8ade17cb1ae3fe9ea1360bf2e1b7c7254d0b9f45
FirstMegaGame4 <firstmegagame4@outlook.fr>
2025-08-16 21:00:51 +0200

    Enhance Header Navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,6 +24,11 @@ const brands = ref([
   }
 ])
 
+const switchNavbarState = (event) => {
+  event.currentTarget.classList.toggle("is-active")
+  event.currentTarget.parentElement.parentElement.children.item(1).classList.toggle("is-active")
+}
+
 const switchDropdownState = (event) => {
   event.currentTarget.classList.toggle("is-active")
 }
@@ -35,26 +40,36 @@ const switchDropdownState = (event) => {
       <RouterLink class="navbar-item" to="/">
         <img src="/src/assets/banner.png" alt="MModding Title">
       </RouterLink>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="switchNavbarState">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-    <div class="navbar-start">
-      <div class="navbar-item has-dropdown" @click="switchDropdownState">
-        <p class="navbar-link">Guides</p>
-        <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="https://wiki.mmodding.com">Mod Wikis</a>
-          <a class="navbar-item" href="https://docs.mmodding.com">Dev Docs</a>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item has-dropdown" @click="switchDropdownState">
+          <p class="navbar-link">Guides</p>
+          <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="https://wiki.mmodding.com">Mod Wikis</a>
+            <a class="navbar-item" href="https://docs.mmodding.com">Dev Docs</a>
+          </div>
+        </div>
+        <!--
+        <RouterLink class="navbar-item" to="/projects">Projects</RouterLink>
+        <RouterLink class="navbar-item" to="/team">Team</RouterLink>
+        <RouterLink class="navbar-item" to="/timeline">Timeline</RouterLink>
+        <RouterLink class="navbar-item" to="/about">About</RouterLink>
+        -->
+      </div>
+      <div class="navbar-end">
+        <div class="socials">
+          <RouterLink v-for="brand in brands" class="navbar-item brand-logo" :to="'/' + brand.id">
+            <i :class="'bi bi-' + brand.icon"/>
+          </RouterLink>
         </div>
       </div>
-      <!--
-      <RouterLink class="navbar-item" to="/projects">Projects</RouterLink>
-      <RouterLink class="navbar-item" to="/team">Team</RouterLink>
-      <RouterLink class="navbar-item" to="/timeline">Timeline</RouterLink>
-      <RouterLink class="navbar-item" to="/about">About</RouterLink>
-      -->
-    </div>
-    <div class="navbar-end">
-      <RouterLink v-for="brand in brands" class="navbar-item brand-logo" :to="'/' + brand.id">
-        <i :class="'bi bi-' + brand.icon"/>
-      </RouterLink>
     </div>
   </nav>
 </template>
@@ -62,6 +77,9 @@ const switchDropdownState = (event) => {
 <style scoped>
 .brand-logo {
   --bulma-navbar-item-img-max-height: 2.0rem;
+}
+.socials {
+  display: flex;
 }
 p {
   user-select: none;
